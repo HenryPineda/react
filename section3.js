cadf519fa02183f40973bd1a1428044058013f26
HenryPineda <pinedahenryandres@gmail.com>
2019-11-07 17:22:49 -0600

    In part 27 we are covering how to install a react app using the node and the npx create-react-app nameOfTheApp command

---
+++ section3.js
@@ -0,0 +1,65 @@
+
+// Section 3 module introduction part 25
+
+//Just introduction to section 3
+
+
+//The build workflow part 26
+
+//No codepen. Set up a local project or local environment.
+
+// Why.
+
+/*
+
+Optimize code
+
+Use Next-Gen Javascript features
+
+Be more productive Css auto prefixing
+
+*/
+
+// How
+
+/*
+
+Use Dependency Management tool like npm or yarn
+
+Use Bundler Recommended Webpack
+
+Use compiler (Nex-Gen Javascript) Babel + Presets
+
+Use a Development Server
+
+*/
+
+// Installing a react app in part 27
+
+/*
+
+Google create react app and find the officil link to the github repository
+
+https://github.com/facebook/create-react-app
+
+Install nodeJS
+
+https://nodejs.org/
+
+We use nodejs for package management and the local server
+
+To create an react app we have to execute the following command
+
+npx create-react-app nameOfTheApp
+
+Alternatively we can use the following command
+
+npx create-react-app nameOfTheApp --scripts-version 1.1.5
+
+
+*/
+
+// part 28
+
+
+
